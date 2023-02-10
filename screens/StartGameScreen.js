ddeb49ebaa809003cec00db6b4b2e821963af91f
Vita Koval <mayevit@hotmail.com>
2023-02-10 15:20:31 +0200

    start screen styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,19 +4,31 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-          <TextInput style={styles.numberInput} maxLength={ 2} keyboardType='number-pad'/>
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#72063c",
+    backgroundColor:'#3b021f',
     borderRadius: 8,
     elevation: 4,
     shadowColor: "black",
@@ -25,16 +37,22 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
   },
   numberInput: {
-      height: 50,
-      width: 50,
-      fontSize: 32,
-    fontWeight: 'bold',
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    fontWeight: "bold",
     borderBottomColor: "#ddb52f",
     borderBottomWidth: 2,
     color: "#ddb52f",
-      marginVertical: 8,
-    textAlign: 'center',
+    marginVertical: 8,
+    textAlign: "center",
   },
+  buttonsContainer: {
+    flexDirection: "row",
+    },
+    buttonContainer: {
+      flex: 1,
+  }
 });
 
 export default StartGameScreen;
